1f65029a79befae946946f6ae0de89be4f47179c
Vincent Lefoulon <vincent.lefoulon@free.fr>
2016-12-29 12:49:20 +0100

    Create LibreOffice project class

--- projectile/__init__.py
+++ projectile/__init__.py
@@ -1,2 +1,3 @@
 from .project import *
+from .libreoffice_project import *
 from .trello_project import *
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ HERE = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_FOLDER = os.path.join(HERE, 'projects')
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def emulate_project_folder():
     try:
         shutil.rmtree(PROJECT_FOLDER)
